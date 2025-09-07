0ed78225c4078c766af006e8ec1366d24cfae1d0
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-09-07 13:14:56 +0100

    feat: configure Redux store with auth and UI reducers

--- Frontend/src/store/store.ts
+++ Frontend/src/store/store.ts
@@ -1,8 +1,13 @@
 // The central store where we plug everything together
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+import uiReducer from './slices/uiSlice';
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        auth: authReducer,
+        ui: uiReducer
+    }
 })
 
 export type RootState = ReturnType<typeof store.getState>;
