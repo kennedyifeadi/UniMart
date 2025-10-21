3f7ade960b2282bd21b5a2858b6bbe40699da224
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-10-21 11:31:24 +0100

    feat: add DynamicBackground component for dynamic image backgrounds

---
+++ Frontend/src/components/home/DynamicBackground.tsx
@@ -0,0 +1,9 @@
+
+export const DynamicBackground = ({image}: {image: string}) => {
+  return (
+    <div className='w-full h-[100dvh] absolute top-0 -z-10'>
+        <div className="w-full h-full absolute top-0 bg-black/55 " ></div>
+      <img src={image} alt="Dynamic Background" className='w-full h-full object-cover' />
+    </div>
+  )
+}
