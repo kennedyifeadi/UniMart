cb7ee5faac10fab50ccdd1034cc925fce19bf3c5
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-10-23 10:34:54 +0100

    feat: update AppRoutes to use direct components for Login and SignUp instead of withMainLayout

--- Frontend/src/routes/AppRoutes.tsx
+++ Frontend/src/routes/AppRoutes.tsx
@@ -12,8 +12,8 @@ import withMainLayout from '../layouts/WithMainLayout'
 export const AppRoutes = () => {
   const HomeWithLayout = withMainLayout(Home);
   const AboutWithLayout = withMainLayout(About);
-  const LoginWithLayout = withMainLayout(Login);
-  const SignUpWithLayout = withMainLayout(SignUp);
+  // const LoginWithLayout = withMainLayout(Login);
+  // const SignUpWithLayout = withMainLayout(SignUp);
   const VendorsWithLayout = withMainLayout(Vendors);
   const BecomeAVendorWithLayout = withMainLayout(BecomeAVendor);
   const NotFoundWithLayout = withMainLayout(NotFound);
@@ -22,8 +22,8 @@ export const AppRoutes = () => {
     <Routes>
       <Route path="/" element={<HomeWithLayout />} />
       <Route path="/about" element={<AboutWithLayout />} />
-      <Route path="/login" element={<LoginWithLayout />} />
-      <Route path="/signup" element={<SignUpWithLayout />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/becomeavendor" element={<BecomeAVendorWithLayout />} />
       <Route path="/vendors" element={<VendorsWithLayout />} />
       <Route path="*" element={<NotFoundWithLayout />} />
