dadb9cca115c069a968671852739a216fffcc02b
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-10-24 01:45:21 +0100

    feat: add Heading2 component for rendering h2 headings with customizable styles

---
+++ Frontend/src/components/headings/Heading2.tsx
@@ -0,0 +1,7 @@
+import type { HeadingProps } from "./Heading1"
+
+export const Heading2 = ({ children, className }: HeadingProps) => {
+  return (
+    <h2 className={`text-2xl md:text-4xl text-center font-bold md:mb-3 ${className}`}>{children}</h2>
+  )
+}
