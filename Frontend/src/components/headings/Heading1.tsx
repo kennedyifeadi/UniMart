2adac35e6cef1f7391e7dabca9ddf614fdeb659b
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-10-24 01:45:03 +0100

    feat: add Heading1 component for rendering headings with customizable styles

---
+++ Frontend/src/components/headings/Heading1.tsx
@@ -0,0 +1,9 @@
+export type HeadingProps = {
+  children: React.ReactNode
+  className?: string
+}
+export const Heading1 = ({ children, className }: HeadingProps) => {
+  return (
+    <h1 className={`text-3xl md:text-5xl text-center font-bold md:mb-3 ${className}`}>{children}</h1>
+  )
+}
