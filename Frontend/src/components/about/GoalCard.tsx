02d188c4b1cd6e65cf14277bbc0be994ca1f46c0
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-10-26 22:46:35 +0100

    feat: refactor GoalCard component to accept props for title, description, and icon

--- Frontend/src/components/about/GoalCard.tsx
+++ Frontend/src/components/about/GoalCard.tsx
@@ -1,6 +1,21 @@
-
-export const GoalCard = () => {
+export interface GoalCardProps {
+  title: string
+  description: string
+  icon: React.ReactNode
+}
+export const GoalCard = ({
+  title,
+  description,
+  icon
+}: GoalCardProps) => {
   return (
-    <div>GoalCard</div>
+    <div className="p-4 rounded-xl shadow-lg hover:shadow-xl duration-300 ease-in-out flex flex-col pt-12 items-center w-[300px] h-[350px]">
+      <div className="flex items-center justify-center mb-2 bg-[#dbeafe] text-[#2563eb] p-4 rounded-full">
+        {icon}
+      </div>
+      <h1 className="font-medium text-[18px] md:text-[20px] mb-5">{title}</h1>
+
+      <p className="text-gray-700 text-[16px] md:text-[18px] px-4 text-center">{description}</p>
+    </div>
   )
 }
