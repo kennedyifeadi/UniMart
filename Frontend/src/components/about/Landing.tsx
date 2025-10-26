0c6007da800f68464b25e57b0a0724ad06a2dd70
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-10-26 22:46:42 +0100

    feat: implement Landing component with dynamic background and content

--- Frontend/src/components/about/Landing.tsx
+++ Frontend/src/components/about/Landing.tsx
@@ -1,6 +1,12 @@
+import { DynamicBackground } from "../home/DynamicBackground"
+import aboutBG from "../../assets/images/UI_About.jpeg"
 
 export const Landing = () => {
   return (
-    <div>Landing</div>
+    <div className="w-full h-[80dvh] flex justify-center items-center relative flex-col text-white mb-10">
+      <DynamicBackground image={aboutBG} />
+      <h1 className="text-[48px] md:text-[72px] font-bold ">About Unimart</h1>
+      <p className="text-center text-[16px] md:text-[24px] w-1/2 text-[#ffffffb1]">The No.1 marketplace connecting UI students with UI businesses. Building a thriving community where students discover, connect, and support each other.</p>
+    </div>
   )
 }
