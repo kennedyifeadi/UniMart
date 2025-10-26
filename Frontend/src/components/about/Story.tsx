64aa2c6f622197107f19f8b723281523c34cc297
Kennedy Ifeadi <kennedyifeadi@gmail.com>
2025-10-26 22:46:50 +0100

    feat: enhance Story component to accept title and description as props

--- Frontend/src/components/about/Story.tsx
+++ Frontend/src/components/about/Story.tsx
@@ -1,6 +1,13 @@
 
-export const Story = () => {
+export interface StoryProps {
+  title: string
+  description: string
+}
+export const Story = ({ title, description }: StoryProps) => {
   return (
-    <div>Story</div>
+    <div className="w-full h-max flex flex-col gap-2">
+      <h2 className=" font-medium md:text-xl text-lg">{title}</h2>
+      <p className="text-gray-700 text-sm md:text-lg">{description}</p>
+    </div>
   )
 }
